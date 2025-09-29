f13062841b2591b4aa27513b47bf9bb5ff449d39
port-orange <jero.a.pardo@gmail.com>
2025-09-29 15:46:30 +0800

    Add fruit and constant vars

--- game.js
+++ game.js
@@ -2,7 +2,18 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const CELL_SIZE = 10;
+const FRUIT_SIZE = 6;
 const DRAW_SPEED = 500;
+const MIN_WIDTH = 0;
+const MIN_HEIGHT = 0;
+const MAX_WIDTH = canvas.clientWidth;
+const MAX_HEIGHT = canvas.clientHeight;
+
+// min and max possible fruit positions
+const MIN_FRUIT_WIDTH = MIN_WIDTH + 2;
+const MAX_FRUIT_WIDTH = MAX_WIDTH - 8;
+const MIN_FRUIT_HEIGHT = MIN_HEIGHT + 2;
+const MAX_FRUIT_HEIGHT = MAX_HEIGHT - 8;
 
 document.addEventListener("keyup", (e) => {
     console.log(e.key);
@@ -26,6 +37,25 @@ document.addEventListener("keyup", (e) => {
     }
 });
 
+class Fruit {
+    constructor() {
+        this.generatePos();
+    }
+
+    generatePos() {
+        this.x = this.generateNum(MIN_FRUIT_WIDTH, MAX_FRUIT_WIDTH);
+        this.y = this.generateNum(MIN_FRUIT_HEIGHT, MAX_FRUIT_HEIGHT);
+    }
+
+    generateNum(min, max) {
+        const numRange = (max - min) / 10;
+        const randomNum = Math.floor(Math.random() * (numRange + 1));
+        const result = min + randomNum * 10;
+
+        return result;
+    }
+}
+
 class Segment {
     constructor(x, y) {
         this.x = x;
@@ -35,11 +65,12 @@ class Segment {
 
 class Snake {
     constructor() {
+        // snake starts with 4 segments
         this.segments = [
-            new Segment(300, 300),
-            new Segment(290, 300),
-            new Segment(280, 300),
-            new Segment(270, 300)
+            new Segment(200, 200),
+            new Segment(190, 200),
+            new Segment(180, 200),
+            new Segment(170, 200)
         ];
         this.x_axis = true;
         this.is_forward = true;
@@ -55,7 +86,7 @@ class Snake {
     }
 }
 
-class collisionChecker {
+class CollisionChecker {
     checkWalls(snake, minBounds, maxBounds) {
         const head = snake.segments[0];
         return (head.x < minBounds || head.x >= maxBounds) || (head.y < minBounds || head.y >= maxBounds);
@@ -76,11 +107,12 @@ class collisionChecker {
 
 
 const snake = new Snake();
-const cc = new collisionChecker();
+const cc = new CollisionChecker();
+const f = new Fruit();
 let gameOver = false;
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    ctx.clearRect(0, 0, MAX_WIDTH, MAX_HEIGHT);
     ctx.fillStyle = "white";
     for (let i = 0; i < snake.segments.length; i++) {
         let segment = snake.segments[i];
@@ -91,30 +123,32 @@ function draw() {
     if (snake.x_axis) {
         if (snake.is_forward) {
             snake.move(segment.x + CELL_SIZE, segment.y);
-            if (cc.checkWalls(snake, 0, 600) || cc.checkBody(snake)) {
+            if (cc.checkWalls(snake, MIN_WIDTH, MAX_WIDTH) || cc.checkBody(snake)) {
                 gameOver = true;
             }
         } else {
             snake.move(segment.x - CELL_SIZE, segment.y);
-            if (cc.checkWalls(snake, 0, 600) || cc.checkBody(snake)) {
+            if (cc.checkWalls(snake, MIN_HEIGHT, MAX_HEIGHT) || cc.checkBody(snake)) {
                 gameOver = true;
             }
         }
     } else {
         if (snake.is_forward) {
             snake.move(segment.x, segment.y - CELL_SIZE);
-            if (cc.checkWalls(snake, 0, 600) || cc.checkBody(snake)) {
+            if (cc.checkWalls(snake, MIN_WIDTH, MAX_WIDTH) || cc.checkBody(snake)) {
                 gameOver = true;
             }
         } else {
             snake.move(segment.x, segment.y + CELL_SIZE);
-            if (cc.checkWalls(snake, 0, 600) || cc.checkBody(snake)) {
+            if (cc.checkWalls(snake, MIN_HEIGHT, MAX_HEIGHT) || cc.checkBody(snake)) {
                 gameOver = true;
             }
         }
     }
 
-    console.log(snake.segments[0].x)
+    // Fruit creation
+    ctx.fillStyle = "red";
+    ctx.fillRect(f.x, f.y, FRUIT_SIZE, FRUIT_SIZE);
 }
 
 draw();
